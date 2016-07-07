39c344dd18f5d3cabe6d36423048f661097a3ffa
ricardofunke <ricardo.funke@liferay.com>
2016-07-07 14:38:42 -0300

    remove LATEST.txt after use (concurrently safe)

--- makemyenv.sh
+++ makemyenv.sh
@@ -397,8 +397,9 @@ fi
 # esac
 
 # Inform puppet the Patching Tool version to download
-wget -q ${HTTP_SERVER}/private/ee/fix-packs/patching-tool/LATEST.txt -P /tmp 
-patching_tool_version="$(cat /tmp/LATEST.txt)" && rm -f /tmp/LATEST.txt
+mkdir /tmp/${ticket}
+wget -q ${HTTP_SERVER}/private/ee/fix-packs/patching-tool/LATEST.txt -P /tmp/${ticket}/
+patching_tool_version="$(cat /tmp/LATEST.txt)" && rm -rf /tmp/${ticket}
 sed -i "s/@@PTVER@@/${patching_tool_version}/" modules/liferay/manifests/init.pp 
 
 # Inform puppet what driver and what patch to install on Liferay
